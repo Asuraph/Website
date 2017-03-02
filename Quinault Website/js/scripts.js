bd84263fdb4c6d7472edc028d454af2abddbb2aa
Asuraph <Asuraph@Gmail.com>
2017-03-02 09:56:58 -0800

    Added some more Javascript
    Just playing with Javascript, trying to get back into the logic and understanding of the language. More or less a refresher. Used a quick and dirty way to fix the scrolling issue for the fixed navbar. Pretty sweet.

--- Quinault Website/js/scripts.js
+++ Quinault Website/js/scripts.js
@@ -1,8 +1,23 @@
-    $(document).ready(function () {
-        $('body').scrollspy({
-            target: '#sideBarNav',
-            offset: 10
-        });
-    });
-    
 
+function count_rabbits() {
+    for(var i=1; i<=3; i++) {
+        // operator + concatenates strings
+        alert("Rabbit "+i+" out of the hat!")
+    }
+}
+
+window.addEventListener("hashchange", function() { scrollBy(0,-50) })
+$(document).ready(function () {
+        
+     $("#sideBarNav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+                }, 500, function(){
+                window.location.hash = hash;
+            });
+         }
+     });
+});

46ab2e54eb0cbd680d018bfa84bfed9c91fa6a8b
Asuraph <Asuraph@Gmail.com>
2017-03-15 16:33:48 -0700

    Redoing Site
    Moving stuff around and attempting to make something that is nicer looking and more interesting.

--- Quinault Website/js/scripts.js
+++ Quinault Website/js/scripts.js
@@ -6,7 +6,15 @@ function count_rabbits() {
         alert("Rabbit "+i+" out of the hat!")
     }
 }
-
+$(document).ready(function () {
+$("#siteNavBar").hide(); //hide your div initially
+    $(window).scroll(function() {
+        // once bottom of div is scrolled past
+        if($(body).scrollTop() >= (500px) {
+            $("#siteNavBar").show(); //reached the desired point -- show div
+        }
+    });
+    });
 //window.addEventListener("hashchange", function() { scrollBy(0,-50) })
 $(document).ready(function () {
         
@@ -28,3 +36,4 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
